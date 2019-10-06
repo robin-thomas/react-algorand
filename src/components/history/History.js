261ca674e1dd21748f515d9d45761e2bc4c8a34b
Robin Thomas <robinthomas2591@gmail.com>
2019-10-06 09:27:49 +0800

    ui improvs

--- src/components/history/History.js
+++ src/components/history/History.js
@@ -1,19 +1,58 @@
 import React from "react";
 
-import { DataConsumer } from "../utils/DataProvider";
+import { Row, Col } from "react-bootstrap";
 
-import Details from "./Details";
 import Transfer from "./Transfer";
+import Detail from "./Detail";
+import Content from "../app/Content";
+
+import { DataConsumer } from "../utils/DataProvider";
+import EmptyRow from "../utils/EmptyRow";
+
+import "./History.css";
 
 const History = props => (
-  <DataConsumer>
-    {ctx => (
-      <div>
-        <Details txs={ctx.txs} />
-        <Transfer />
-      </div>
-    )}
-  </DataConsumer>
+  <div>
+    <Content header="History">
+      <Row className="algorand-history-header">
+        <Col>
+          <span>Transactions in this session</span>
+        </Col>
+      </Row>
+      <DataConsumer>
+        {ctx =>
+          ctx.txs && ctx.txs.length > 0 ? (
+            <div>
+              {Object.keys(ctx.txs).map((key, index) => (
+                <Detail key={index} tx={ctx.txs[key]} />
+              ))}
+              <EmptyRow />
+            </div>
+          ) : (
+            <div>
+              <EmptyRow />
+              <Row className="algorand-history-row">
+                <Col>
+                  <i class="fas fa-coins"></i>
+                  <p
+                    style={{
+                      fontWeight: "400",
+                      fontSize: "14px",
+                      lineHeight: "15px"
+                    }}
+                  >
+                    Nothing here! Make one transaction :)
+                  </p>
+                </Col>
+              </Row>
+              <EmptyRow />
+            </div>
+          )
+        }
+      </DataConsumer>
+    </Content>
+    <Transfer />
+  </div>
 );
 
 export default History;
